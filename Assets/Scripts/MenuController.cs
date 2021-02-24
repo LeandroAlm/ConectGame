f2d788a2486f513f60a3ea2af9b4738b80701257
LeandroAlm <leo11leo55@yahoo.com>
2021-02-24 19:18:18 +0000

    First commit
    Basic menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    static int LevelToLoad;
+    
+
+    public static int GetLevelToLoad()
+    {
+        return MenuController.LevelToLoad;
+    }
+
+    public void MenuOnButtonClickLevel(int level_id)
+    {
+        MenuController.LevelToLoad = level_id;
+        SceneManager.LoadScene("Game");
+        //SceneManager.UnloadScene("Menu");
+    }
+}
